eace871dc9e5aa3dd61eda540598d38e9119e089
FlorVk <florvankuyk@hotmail.com>
2022-08-27 15:30:27 +0200

    Feat: print users out with a birthday

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// FRONTEND
+
+const base_url = "http://localhost:3000";
+const birthday = "1998-11-25";
+
+
+/* redirect if not logged in */
+if (!localStorage.getItem("token")) {
+    window.location.href = "login.html";
+}
+
+/* fetch all users on load */
+fetch(base_url + "/api/v1/bday/" + birthday, {
+    'headers': {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+}).then(result => {
+    return result.json();
+}).then(json => {
+    json.data.forEach(user => {
+        let userData = `<div class="user__username">${user.username}</div>`;
+        document.querySelector(".users__body").insertAdjacentHTML('beforeend', userData);
+    });
+
+
+}).catch(err => {
+    console.log(err);
+    console.log("Something went wrong")
+});
+
+
+// simple logout functionality
+document.querySelector(".option__logout").addEventListener("click", e => {
+    localStorage.removeItem("token");
+    window.location.href = "login.html";
+    e.preventDefault();
+});
